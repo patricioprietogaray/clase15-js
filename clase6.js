464da27a7de282f1034b6a1bed67f73eb12a06cf
patricioprietogaray <patricioprietogaray@gmail.com>
2022-07-10 12:39:03 -0300

    master

--- clase6.js
+++ clase6.js
@@ -501,3 +501,43 @@ yoda.hablar();
 let obiWan = new Jedi('Obi Wan', 'Azul');
 console.log(obiWan);
 obiWan.hablar();
+
+
+
+///////
+// Clases
+
+
+
+class Jedi { //declaro la clase
+    constructor(nombre, color) {  //son los valores que voy a pasar (no son fijos)
+        // propiedades
+        this.arma = 'Sable de luz'; //valor fijo
+        this.colorSableLuz = color; //pasado por el constructor
+        this.poder = 'La fuerza';   //valor fijo
+        this.nombre = nombre;       //pasado por el constructor
+        // metodo
+        this.hablar = function () {
+            console.log(`${this.nombre}: Que la fuerza te acompañe!.`);
+        };
+    }
+}
+
+//creacion de una instancia
+let j = new Jedi();
+
+// imprimir en la consola
+console.log(j);
+j.arma='pistola laser';
+console.log(j);
+console.log('no se tiene acceso a la funcion padre en forma directa');
+console.log(Jedi.arma)
+
+// se aplica la herencia de los valores fijos de la funcion
+// constructora y paso los parámetros de lo que varia.
+let yoda = new Jedi('Yoda', 'Verde'); //parámetros a pasar por el constructor
+console.log(yoda);
+yoda.hablar();
+let obiWan = new Jedi('Obi Wan', 'Azul');
+console.log(obiWan);
+obiWan.hablar();
